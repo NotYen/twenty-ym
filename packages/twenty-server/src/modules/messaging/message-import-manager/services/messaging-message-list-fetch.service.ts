twentyhq/twenty

commit 8b5eb04b07cd81864b3dd4e55b4ecb0fb09f401c

Refactor mail folders (#13302)
## Context

We would like to start leveraging more messageFolders during messageSync
which would allow users to select folders they want to sync on their
mailbox. MessageFolder is an abstraction that means folder for
Microsoft, labels for Gmail, not supported for IMAP.


## What

1) We do not aim to build a FE now but we would like to take it into a
consideration if they have been modified through API. So **out of
scope**
2) MessageFolders will be synced regularly from Gmail, Microsoft. This
is currently partially done and improving it is **out of scope**
3) Change: we were having two synchronization mechanism so far:
FULL_SYNC (first time, or when no cursor is present) and PARTIAL_SYNC
(when we have a cursor, we can fetch the diff since the last pull). This
Full vs Partial mode was a high level concept. We now think it's an
driver implementation detail (and can be inferred from the existence of
a cursor). Why making this change now: as we are trying to pull several
folders, a folder could need a full sync if it had no cursor, and
another a partial if it has one. It does not make sense anymore to have
this full vs partial difference at MessageChannel level
4) Once we are sure this work, we can start syncing different folders on
Gmail side (the case for the user it to be able to fetch Promotion
label)

## Implementation strategy

1) Re-use PartialMessageList implementation / API and rename it to
MessageList at it's more complete
2) re-use driver level fullMessageList methods and call them
messageListWithoutCursor
3) make sure that these method are folder specific

## Tests

### Gmail

- Fresh fetch (without cursor): OK
- Message import: OK
- Additional fetch (with messageChannel cursor): OK

### Microsoft

- Fresh fetch (without cursor): OK
- Message import: OK
- Additional fetch (with messageChannel cursor): OK

### Imap

- Fresh fetch (without cursor): OK
- Message import: OK
- Additional fetch (with messageChannel cursor): OK

packages/twenty-server/src/modules/messaging/message-import-manager/services/messaging-message-list-fetch.service.ts
@@ -6,7 +6,6 @@ import { InjectCacheStorage } from 'src/engine/core-modules/cache-storage/decora
 import { CacheStorageService } from 'src/engine/core-modules/cache-storage/services/cache-storage.service';
 import { CacheStorageNamespace } from 'src/engine/core-modules/cache-storage/types/cache-storage-namespace.enum';
 import { TwentyORMManager } from 'src/engine/twenty-orm/twenty-orm.manager';
-import { ConnectedAccountWorkspaceEntity } from 'src/modules/connected-account/standard-objects/connected-account.workspace-entity';
 import { MessageChannelSyncStatusService } from 'src/modules/messaging/common/services/message-channel-sync-status.service';
 import { MessageChannelMessageAssociationWorkspaceEntity } from 'src/modules/messaging/common/standard-objects/message-channel-message-association.workspace-entity';
 import { MessageChannelWorkspaceEntity } from 'src/modules/messaging/common/standard-objects/message-channel.workspace-entity';
@@ -17,15 +16,14 @@ import {
   MessageImportExceptionHandlerService,
   MessageImportSyncStep,
 } from 'src/modules/messaging/message-import-manager/services/messaging-import-exception-handler.service';
-
 @Injectable()
-export class MessagingPartialMessageListFetchService {
+export class MessagingMessageListFetchService {
   constructor(
     @InjectCacheStorage(CacheStorageNamespace.ModuleMessaging)
     private readonly cacheStorage: CacheStorageService,
-    private readonly messagingGetMessageListService: MessagingGetMessageListService,
     private readonly messageChannelSyncStatusService: MessageChannelSyncStatusService,
     private readonly twentyORMManager: TwentyORMManager,
+    private readonly messagingGetMessageListService: MessagingGetMessageListService,
     private readonly messageImportErrorHandlerService: MessageImportExceptionHandlerService,
     private readonly messagingMessageCleanerService: MessagingMessageCleanerService,
     private readonly messagingCursorService: MessagingCursorService,
@@ -33,59 +31,64 @@ export class MessagingPartialMessageListFetchService {
 
   public async processMessageListFetch(
     messageChannel: MessageChannelWorkspaceEntity,
-    connectedAccount: ConnectedAccountWorkspaceEntity,
     workspaceId: string,
-  ): Promise<void> {
+  ) {
     try {
       await this.messageChannelSyncStatusService.markAsMessagesListFetchOngoing(
         [messageChannel.id],
       );
 
-      const messageChannelRepository =
-        await this.twentyORMManager.getRepository<MessageChannelWorkspaceEntity>(
-          'messageChannel',
-        );
-
-      await messageChannelRepository.update(
-        {
-          id: messageChannel.id,
-        },
-        {
-          throttleFailureCount: 0,
-        },
-      );
-
-      const partialMessageLists =
-        await this.messagingGetMessageListService.getPartialMessageLists(
+      const messageLists =
+        await this.messagingGetMessageListService.getMessageLists(
           messageChannel,
         );
 
-      for (const partialMessageList of partialMessageLists) {
-        const {
-          messageExternalIds,
-          messageExternalIdsToDelete,
-          previousSyncCursor,
-          nextSyncCursor,
-          folderId,
-        } = partialMessageList;
+      const isEmptyMailbox = messageLists.some(
+        (messageList) => messageList.messageExternalIds.length === 0,
+      );
 
-        const isPartialImportFinished = this.isPartialImportFinished(
-          previousSyncCursor,
-          nextSyncCursor,
+      if (isEmptyMailbox) {
+        await this.messageChannelSyncStatusService.resetAndScheduleMessageListFetch(
+          [messageChannel.id],
+          workspaceId,
         );
 
-        if (isPartialImportFinished) {
-          continue;
-        }
+        return;
+      }
 
-        await this.cacheStorage.setAdd(
-          `messages-to-import:${workspaceId}:${messageChannel.id}`,
-          messageExternalIds,
-        );
+      for (const messageList of messageLists) {
+        const { messageExternalIds, nextSyncCursor, folderId } = messageList;
 
         const messageChannelMessageAssociationRepository =
           await this.twentyORMManager.getRepository<MessageChannelMessageAssociationWorkspaceEntity>(
             'messageChannelMessageAssociation',
+          );
+
+        const existingMessageChannelMessageAssociations =
+          await messageChannelMessageAssociationRepository.find({
+            where: {
+              messageChannelId: messageChannel.id,
+            },
+          });
+
+        const existingMessageChannelMessageAssociationsExternalIds =
+          existingMessageChannelMessageAssociations.map(
+            (messageChannelMessageAssociation) =>
+              messageChannelMessageAssociation.messageExternalId,
+          );
+
+        const messageExternalIdsToImport = messageExternalIds.filter(
+          (messageExternalId) =>
+            !existingMessageChannelMessageAssociationsExternalIds.includes(
+              messageExternalId,
+            ),
+        );
+
+        const messageExternalIdsToDelete =
+          existingMessageChannelMessageAssociationsExternalIds.filter(
+            (existingMessageCMAExternalId) =>
+              existingMessageCMAExternalId &&
+              !messageExternalIds.includes(existingMessageCMAExternalId),
           );
 
         if (messageExternalIdsToDelete.length) {
@@ -99,27 +102,18 @@ export class MessagingPartialMessageListFetchService {
           );
         }
 
+        if (messageExternalIdsToImport.length) {
+          await this.cacheStorage.setAdd(
+            `messages-to-import:${workspaceId}:${messageChannel.id}`,
+            messageExternalIdsToImport,
+          );
+        }
+
         await this.messagingCursorService.updateCursor(
           messageChannel,
           nextSyncCursor,
           folderId,
         );
-      }
-
-      const isPartialImportFinishedForAllFolders = partialMessageLists.every(
-        (partialMessageList) =>
-          this.isPartialImportFinished(
-            partialMessageList.previousSyncCursor,
-            partialMessageList.nextSyncCursor,
-          ),
-      );
-
-      if (isPartialImportFinishedForAllFolders) {
-        await this.messageChannelSyncStatusService.markAsCompletedAndSchedulePartialMessageListFetch(
-          [messageChannel.id],
-        );
-
-        return;
       }
 
       await this.messageChannelSyncStatusService.scheduleMessagesImport([
@@ -128,17 +122,10 @@ export class MessagingPartialMessageListFetchService {
     } catch (error) {
       await this.messageImportErrorHandlerService.handleDriverException(
         error,
-        MessageImportSyncStep.PARTIAL_MESSAGE_LIST_FETCH,
+        MessageImportSyncStep.FULL_MESSAGE_LIST_FETCH,
         messageChannel,
         workspaceId,
       );
     }
-  }
-
-  private isPartialImportFinished(
-    previousSyncCursor: string,
-    nextSyncCursor: string,
-  ): boolean {
-    return previousSyncCursor === nextSyncCursor;
   }
 }

packages/twenty-server/src/modules/messaging/message-import-manager/services/messaging-message-list-fetch.service.spec.ts
@@ -10,12 +10,12 @@ import { MessageChannelWorkspaceEntity } from 'src/modules/messaging/common/stan
 import { MessageFolderWorkspaceEntity } from 'src/modules/messaging/common/standard-objects/message-folder.workspace-entity';
 import { MessagingMessageCleanerService } from 'src/modules/messaging/message-cleaner/services/messaging-message-cleaner.service';
 import { MessagingCursorService } from 'src/modules/messaging/message-import-manager/services/messaging-cursor.service';
-import { MessagingFullMessageListFetchService } from 'src/modules/messaging/message-import-manager/services/messaging-full-message-list-fetch.service';
 import { MessagingGetMessageListService } from 'src/modules/messaging/message-import-manager/services/messaging-get-message-list.service';
 import { MessageImportExceptionHandlerService } from 'src/modules/messaging/message-import-manager/services/messaging-import-exception-handler.service';
+import { MessagingMessageListFetchService } from 'src/modules/messaging/message-import-manager/services/messaging-message-list-fetch.service';
 
-describe('MessagingFullMessageListFetchService', () => {
-  let messagingFullMessageListFetchService: MessagingFullMessageListFetchService;
+describe('MessagingMessageListFetchService', () => {
+  let messagingMessageListFetchService: MessagingMessageListFetchService;
   let messagingGetMessageListService: MessagingGetMessageListService;
   let messageChannelSyncStatusService: MessageChannelSyncStatusService;
   let twentyORMManager: TwentyORMManager;
@@ -72,7 +72,7 @@ describe('MessagingFullMessageListFetchService', () => {
 
     const module: TestingModule = await Test.createTestingModule({
       providers: [
-        MessagingFullMessageListFetchService,
+        MessagingMessageListFetchService,
         {
           provide: CacheStorageNamespace.ModuleMessaging,
           useValue: {
@@ -82,12 +82,11 @@ describe('MessagingFullMessageListFetchService', () => {
         {
           provide: MessagingGetMessageListService,
           useValue: {
-            getFullMessageLists: jest
+            getMessageLists: jest
               .fn()
-              .mockImplementation((messageChannel) => {
+              .mockImplementation(({ connectedAccount }) => {
                 if (
-                  messageChannel.connectedAccount.provider ===
-                  ConnectedAccountProvider.GOOGLE
+                  connectedAccount.provider === ConnectedAccountProvider.GOOGLE
                 ) {
                   return [
                     {
@@ -162,9 +161,9 @@ describe('MessagingFullMessageListFetchService', () => {
       ],
     }).compile();
 
-    messagingFullMessageListFetchService =
-      module.get<MessagingFullMessageListFetchService>(
-        MessagingFullMessageListFetchService,
+    messagingMessageListFetchService =
+      module.get<MessagingMessageListFetchService>(
+        MessagingMessageListFetchService,
       );
     messagingGetMessageListService = module.get<MessagingGetMessageListService>(
       MessagingGetMessageListService,
@@ -180,7 +179,7 @@ describe('MessagingFullMessageListFetchService', () => {
   });
 
   it('should process Microsoft message list fetch correctly', async () => {
-    await messagingFullMessageListFetchService.processMessageListFetch(
+    await messagingMessageListFetchService.processMessageListFetch(
       mockMicrosoftMessageChannel,
       workspaceId,
     );
@@ -189,9 +188,9 @@ describe('MessagingFullMessageListFetchService', () => {
       messageChannelSyncStatusService.markAsMessagesListFetchOngoing,
     ).toHaveBeenCalledWith([mockMicrosoftMessageChannel.id]);
 
-    expect(
-      messagingGetMessageListService.getFullMessageLists,
-    ).toHaveBeenCalledWith(mockMicrosoftMessageChannel);
+    expect(messagingGetMessageListService.getMessageLists).toHaveBeenCalledWith(
+      mockMicrosoftMessageChannel,
+    );
 
     expect(twentyORMManager.getRepository).toHaveBeenCalledWith(
       'messageChannelMessageAssociation',
@@ -209,7 +208,7 @@ describe('MessagingFullMessageListFetchService', () => {
   });
 
   it('should process Google message list fetch correctly', async () => {
-    await messagingFullMessageListFetchService.processMessageListFetch(
+    await messagingMessageListFetchService.processMessageListFetch(
       mockGoogleMessageChannel,
       workspaceId,
     );
@@ -218,9 +217,9 @@ describe('MessagingFullMessageListFetchService', () => {
       messageChannelSyncStatusService.markAsMessagesListFetchOngoing,
     ).toHaveBeenCalledWith([mockGoogleMessageChannel.id]);
 
-    expect(
-      messagingGetMessageListService.getFullMessageLists,
-    ).toHaveBeenCalledWith(mockGoogleMessageChannel);
+    expect(messagingGetMessageListService.getMessageLists).toHaveBeenCalledWith(
+      mockGoogleMessageChannel,
+    );
 
     expect(twentyORMManager.getRepository).toHaveBeenCalledWith(
       'messageChannelMessageAssociation',

packages/twenty-server/src/modules/messaging/blocklist-manager/jobs/messaging-blocklist-reimport-messages.job.ts
@@ -54,7 +54,7 @@ export class BlocklistReimportMessagesJob {
         },
       });
 
-      await this.messagingChannelSyncStatusService.resetAndScheduleFullMessageListFetch(
+      await this.messagingChannelSyncStatusService.resetAndScheduleMessageListFetch(
         messageChannels.map((messageChannel) => messageChannel.id),
         workspaceId,
       );

packages/twenty-server/src/modules/messaging/common/services/message-channel-sync-status.service.ts
@@ -27,7 +27,7 @@ export class MessageChannelSyncStatusService {
     private readonly metricsService: MetricsService,
   ) {}
 
-  public async scheduleFullMessageListFetch(messageChannelIds: string[]) {
+  public async scheduleMessageListFetch(messageChannelIds: string[]) {
     if (!messageChannelIds.length) {
       return;
     }
@@ -39,21 +39,6 @@ export class MessageChannelSyncStatusService {
 
     await messageChannelRepository.update(messageChannelIds, {
       syncStage: MessageChannelSyncStage.FULL_MESSAGE_LIST_FETCH_PENDING,
-    });
-  }
-
-  public async schedulePartialMessageListFetch(messageChannelIds: string[]) {
-    if (!messageChannelIds.length) {
-      return;
-    }
-
-    const messageChannelRepository =
-      await this.twentyORMManager.getRepository<MessageChannelWorkspaceEntity>(
-        'messageChannel',
-      );
-
-    await messageChannelRepository.update(messageChannelIds, {
-      syncStage: MessageChannelSyncStage.PARTIAL_MESSAGE_LIST_FETCH_PENDING,
     });
   }
 
@@ -72,7 +57,7 @@ export class MessageChannelSyncStatusService {
     });
   }
 
-  public async resetAndScheduleFullMessageListFetch(
+  public async resetAndScheduleMessageListFetch(
     messageChannelIds: string[],
     workspaceId: string,
   ) {
@@ -97,7 +82,7 @@ export class MessageChannelSyncStatusService {
       throttleFailureCount: 0,
     });
 
-    await this.scheduleFullMessageListFetch(messageChannelIds);
+    await this.scheduleMessageListFetch(messageChannelIds);
   }
 
   public async resetSyncStageStartedAt(messageChannelIds: string[]) {
@@ -132,7 +117,7 @@ export class MessageChannelSyncStatusService {
     });
   }
 
-  public async markAsCompletedAndSchedulePartialMessageListFetch(
+  public async markAsCompletedAndScheduleMessageListFetch(
     messageChannelIds: string[],
   ) {
     if (!messageChannelIds.length) {
@@ -146,7 +131,7 @@ export class MessageChannelSyncStatusService {
 
     await messageChannelRepository.update(messageChannelIds, {
       syncStatus: MessageChannelSyncStatus.ACTIVE,
-      syncStage: MessageChannelSyncStage.PARTIAL_MESSAGE_LIST_FETCH_PENDING,
+      syncStage: MessageChannelSyncStage.FULL_MESSAGE_LIST_FETCH_PENDING,
       throttleFailureCount: 0,
       syncStageStartedAt: null,
       syncedAt: new Date().toISOString(),

packages/twenty-server/src/modules/messaging/common/standard-objects/message-channel.workspace-entity.ts
@@ -31,8 +31,8 @@ export enum MessageChannelSyncStatus {
 }
 
 export enum MessageChannelSyncStage {
-  FULL_MESSAGE_LIST_FETCH_PENDING = 'FULL_MESSAGE_LIST_FETCH_PENDING',
-  PARTIAL_MESSAGE_LIST_FETCH_PENDING = 'PARTIAL_MESSAGE_LIST_FETCH_PENDING',
+  FULL_MESSAGE_LIST_FETCH_PENDING = 'FULL_MESSAGE_LIST_FETCH_PENDING', // TODO: rename to MESSAGE_LIST_FETCH_PENDING
+  PARTIAL_MESSAGE_LIST_FETCH_PENDING = 'PARTIAL_MESSAGE_LIST_FETCH_PENDING', // TODO: to be removed, deprecated
   MESSAGE_LIST_FETCH_ONGOING = 'MESSAGE_LIST_FETCH_ONGOING',
   MESSAGES_IMPORT_PENDING = 'MESSAGES_IMPORT_PENDING',
   MESSAGES_IMPORT_ONGOING = 'MESSAGES_IMPORT_ONGOING',
@@ -291,13 +291,13 @@ export class MessageChannelWorkspaceEntity extends BaseWorkspaceEntity {
     icon: 'IconStatusChange',
     options: [
       {
-        value: MessageChannelSyncStage.FULL_MESSAGE_LIST_FETCH_PENDING,
+        value: MessageChannelSyncStage.FULL_MESSAGE_LIST_FETCH_PENDING, // TODO: Rename to MESSAGE_LIST_FETCH_PENDING
         label: 'Full messages list fetch pending',
         position: 0,
         color: 'blue',
       },
       {
-        value: MessageChannelSyncStage.PARTIAL_MESSAGE_LIST_FETCH_PENDING,
+        value: MessageChannelSyncStage.PARTIAL_MESSAGE_LIST_FETCH_PENDING, // TODO: Deprecate
         label: 'Partial messages list fetch pending',
         position: 1,
         color: 'blue',

packages/twenty-server/src/modules/messaging/message-import-manager/crons/jobs/messaging-message-list-fetch.cron.job.ts
@@ -52,6 +52,7 @@ export class MessagingMessageListFetchCronJob {
           activeWorkspace.id,
         );
 
+        // TODO: deprecate looking for FULL_MESSAGE_LIST_FETCH_PENDING as we introduce MESSAGE_LIST_FETCH_PENDING
         const messageChannels = await mainDataSource.query(
           `SELECT * FROM ${schemaName}."messageChannel" WHERE "isSyncEnabled" = true AND "syncStage" IN ('${MessageChannelSyncStage.PARTIAL_MESSAGE_LIST_FETCH_PENDING}', '${MessageChannelSyncStage.FULL_MESSAGE_LIST_FETCH_PENDING}')`,
         );

packages/twenty-server/src/modules/messaging/message-import-manager/drivers/exceptions/message-network.exception.ts
@@ -4,4 +4,5 @@ export enum MessageNetworkExceptionCode {
   ECONNABORTED = 'ECONNABORTED',
   ETIMEDOUT = 'ETIMEDOUT',
   ERR_NETWORK = 'ERR_NETWORK',
+  EHOSTUNREACH = 'EHOSTUNREACH',
 }

packages/twenty-server/src/modules/messaging/message-import-manager/drivers/gmail/constants/messaging-gmail-excluded-categories.ts
@@ -1,6 +1,8 @@
+import { GmailDefaultMessageCategory } from 'src/modules/messaging/message-import-manager/drivers/gmail/types/gmail-default-message-category.type';
+
 export const MESSAGING_GMAIL_EXCLUDED_CATEGORIES = [
-  'promotions',
-  'social',
-  'forums',
-  'updates',
+  GmailDefaultMessageCategory.promotions,
+  GmailDefaultMessageCategory.forums,
+  GmailDefaultMessageCategory.social,
+  GmailDefaultMessageCategory.updates,
 ];

NotYen/twenty-ym

commit 049b0d42db72466c26377a3c974cf37e4e3aaa7c

Fix critical bug in MessagingMessageListFetchService (#13350)
## Bug description

All old messages were deleted after the first partial sync because the
diff between the existing messages and the messages returned from the
fetch was done considering that it was always a full sync (ie, that we
always retrieve the full list of message ids). But in a partial sync,
only the new messages appear in the list.

This bug was introduced by https://github.com/twentyhq/twenty/pull/13302
when trying to merge the logic between the full sync and the partial
sync.

## Fix

The fix is to adapt the behavior to the type of sync, by looking at the
presence of the sync cursor.

---------

Co-authored-by: Guillim <guillim@users.noreply.github.com>

packages/twenty-server/src/modules/messaging/message-import-manager/services/messaging-message-list-fetch.service.ts
@@ -57,7 +57,13 @@ export class MessagingMessageListFetchService {
       }
 
       for (const messageList of messageLists) {
-        const { messageExternalIds, nextSyncCursor, folderId } = messageList;
+        const {
+          messageExternalIds,
+          nextSyncCursor,
+          folderId,
+          messageExternalIdsToDelete,
+          previousSyncCursor,
+        } = messageList;
 
         const messageChannelMessageAssociationRepository =
           await this.twentyORMManager.getRepository<MessageChannelMessageAssociationWorkspaceEntity>(
@@ -84,17 +90,25 @@ export class MessagingMessageListFetchService {
             ),
         );
 
-        const messageExternalIdsToDelete =
-          existingMessageChannelMessageAssociationsExternalIds.filter(
-            (existingMessageCMAExternalId) =>
-              existingMessageCMAExternalId &&
-              !messageExternalIds.includes(existingMessageCMAExternalId),
-          );
+        const isFullSync = !previousSyncCursor;
 
-        if (messageExternalIdsToDelete.length) {
+        const additionalMessageExternalIdsToDelete = isFullSync
+          ? existingMessageChannelMessageAssociationsExternalIds.filter(
+              (existingMessageCMAExternalId) =>
+                existingMessageCMAExternalId &&
+                !messageExternalIds.includes(existingMessageCMAExternalId),
+            )
+          : [];
+
+        const allMessageExternalIdsToDelete = [
+          ...messageExternalIdsToDelete,
+          ...additionalMessageExternalIdsToDelete,
+        ];
+
+        if (allMessageExternalIdsToDelete.length) {
           await messageChannelMessageAssociationRepository.delete({
             messageChannelId: messageChannel.id,
-            messageExternalId: In(messageExternalIdsToDelete),
+            messageExternalId: In(allMessageExternalIdsToDelete),
           });
 
           await this.messagingMessageCleanerService.cleanWorkspaceThreads(

packages/twenty-server/src/modules/messaging/message-import-manager/services/messaging-message-list-fetch.service.spec.ts
@@ -97,6 +97,8 @@ describe('MessagingMessageListFetchService', () => {
                       ],
                       nextSyncCursor: 'new-google-history-id',
                       folderId: undefined,
+                      messageExternalIdsToDelete: [],
+                      previousSyncCursor: 'google-sync-cursor',
                     },
                   ];
                 } else {
@@ -109,6 +111,8 @@ describe('MessagingMessageListFetchService', () => {
                       ],
                       nextSyncCursor: 'new-sync-cursor',
                       folderId: 'inbox-folder-id',
+                      messageExternalIdsToDelete: [],
+                      previousSyncCursor: 'inbox-sync-cursor',
                     },
                   ];
                 }